5462a8cefef9af0ab9d11f1e518f9d939a5d90c2
Panashe Rusakaniko <panasherussy10@gmail.com>
2022-02-01 11:21:46 +0200

    File with cvs script

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+extending the Python script to export data in the CSV format.
+"""
+
+import requests
+from sys import argv
+import csv
+
+if __name__ == "__main__":
+    data = requests.get('https://jsonplaceholder.typicode.com/todos/').json()
+    data2 = requests.get('https://jsonplaceholder.typicode.com/users').json()
+
+    for i in data2:
+        if i['id'] == int(argv[1]):
+            EMPLOYEE_NAME = i['username']
+
+    with open(argv[1] + '.csv', 'w', newline='') as file:
+        wrt = csv.writer(file, quoting=csv.QUOTE_ALL)
+
+        for i in data:
+            row = []
+            if i['userId'] == int(argv[1]):
+                row.append(i['userId'])
+                row.append(EMPLOYEE_NAME)
+                row.append(i['completed'])
+                row.append(i['title'])
+                wrt.writerow(row)
